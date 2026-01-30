6e8cb297c1ff05c3fe5ba974a823c38450e8bf49
lolahola783 <lolahola783@gmail.com>
2026-01-30 20:32:51 +0530

    Fix CORS preflight: move catch-all OPTIONS handler with CORS middleware

--- backend/src/index.js
+++ backend/src/index.js
@@ -32,7 +32,7 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.use(express.json());
 
-// Debug: Log CORS requests and add catch-all OPTIONS handler
+// Debug: Log CORS requests
 app.use((req, res, next) => {
   console.log('CORS Debug:', {
     origin: req.headers.origin,
@@ -43,10 +43,8 @@ app.use((req, res, next) => {
   next();
 });
 
-app.options('*', (req, res) => {
-  res.header('Access-Control-Allow-Origin', req.headers.origin || '*');
-  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+// Catch-all OPTIONS handler for CORS preflight
+app.options('*', cors(corsOptions), (req, res) => {
   res.sendStatus(200);
 });
 
